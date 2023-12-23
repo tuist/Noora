4e618299c5a63acaccad9871d875b9912d3b7d39
Pedro <pedro@pepicrft.me>
2023-12-23 11:34:14 +0100

    Add missing dependency

--- Package.swift
+++ Package.swift
@@ -6,14 +6,14 @@ import PackageDescription
 let package = Package(
     name: "SwiftTerminal",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftTerminal",
             targets: ["SwiftTerminal"]),
+        .executable(name: "swift-terminal",
+                    targets: ["swift-terminal"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
+        .target(name: "swift-terminal", dependencies: ["SwiftTerminal"]),
         .target(
             name: "SwiftTerminal"),
         .testTarget(
