b50ecb1ebbe5daa5c10769311624d242e91e12c7
Pedro <pedro@pepicrft.me>
2023-12-25 10:20:14 +0100

    Build and test with SPM and Tuist across OSs

--- Tests/SwiftTerminalTests/SwiftTerminalTests.swift
+++ Tests/SwiftTerminalTests/SwiftTerminalTests.swift
@@ -1,7 +1,7 @@
 import XCTest
-@testable import TerminalUI
+@testable import SwiftTerminal
 
-final class TerminalUITests: XCTestCase {
+final class SwiftTerminalTests: XCTestCase {
     func testExample() throws {
         // XCTest Documentation
         // https://developer.apple.com/documentation/xctest
