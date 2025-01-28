b39978197d213726f6530433ef6db5ef46ba9e7e
Pedro <pedro@pepicrft.me>
2025-01-28 15:57:47 +0100

    Remove unused dependencies
    CombineX and CXFoundation bring large dependency trees so it's better to
    avoid them. We are only using a small functionality from them, which is
    something we could mimic with Foundation primitives.

--- Package.swift
+++ Package.swift
@@ -19,21 +19,21 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/onevcat/Rainbow", .upToNextMajor(from: "4.1.0")),
-        .package(url: "https://github.com/cx-org/CombineX", .upToNextMajor(from: "0.4.0")),
-        .package(url: "https://github.com/reddavis/Asynchrone", .upToNextMajor(from: "0.22.0")),
-        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMajor(from: "1.5.0")),
+        .package(
+            url: "https://github.com/apple/swift-argument-parser", .upToNextMajor(from: "1.5.0")
+        ),
     ],
     targets: [
         .executableTarget(
             name: "examples-cli",
-            dependencies: ["Noora", .product(name: "ArgumentParser", package: "swift-argument-parser")]
+            dependencies: [
+                "Noora", .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ]
         ),
         .target(
             name: "Noora",
             dependencies: [
                 "Rainbow",
-                "CombineX",
-                "Asynchrone",
             ],
             swiftSettings: [
                 .define("MOCKING", .when(configuration: .debug)),
