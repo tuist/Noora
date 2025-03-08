4e342e4f901086388290cd5f12d31e718aa9e3bb
Nikita Vasilev <nv3212@gmail.com>
2025-03-08 14:58:33 +0400

    Implement the `Validator` object that validates input with different rules

---
+++ Sources/Noora/Validator/Validator.swift
@@ -0,0 +1,29 @@
+/// A struct responsible for validating input data against specific rules.
+struct Validator {
+    
+    /// Validates the input against a single validation rule.
+    ///
+    /// - Parameters:
+    ///   - input: The data that needs to be validated.
+    ///   - rule: A validation rule to apply on the input.
+    ///
+    /// - Returns: A `ValidationResult` indicating whether the input is valid or invalid, including the associated errors if any.
+    func validate<Rule: ValidatableRule>(input: Rule.Input, rule: Rule) -> ValidationResult {
+        validate(input: input, rules: [AnyValidationRule(validationRule: rule)])
+    }
+
+    /// Validates the input against a list of validation rules.
+    ///
+    /// - Parameters:
+    ///   - input: The data that needs to be validated.
+    ///   - rules: A list of validation rules to apply on the input.
+    ///
+    /// - Returns: A `ValidationResult` indicating whether the input is valid or invalid, including the associated errors if any.
+    func validate<Input>(input: Input, rules: [AnyValidationRule<Input>]) -> ValidationResult {
+        let errors = rules
+            .filter { $0.validate(input: input) }
+            .map { $0.error }
+
+        return errors.isEmpty ? .valid : .invalid(errors: errors)
+    }
+}
