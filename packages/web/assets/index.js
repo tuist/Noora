4a95b15795c8fc8c54bc1aa681830582aabe3d97
Christoph Schmatzler <christoph@tuist.dev>
2025-05-30 16:11:19 +0200

    up
    Signed-off-by: Christoph Schmatzler <christoph@tuist.dev>

--- packages/web/assets/index.js
+++ packages/web/assets/index.js
@@ -1,15 +1,31 @@
 import "./index.css";
 
-export { PlaceCursorAtEnd } from "./js/hooks.js";
-export { default as NooraAvatar } from "./js/Avatar.js";
-export { default as NooraChart } from "./js/Chart.js";
-export { default as NooraCheckbox } from "./js/Checkbox.js";
-export { default as NooraCollapsible } from "./js/Collapsible.js";
-export { default as NooraDigitInput } from "./js/DigitInput.js";
-export { default as NooraDropdown } from "./js/Dropdown.js";
-export { default as NooraModal } from "./js/Modal.js";
-export { default as NooraPopover } from "./js/Popover.js";
-export { default as NooraSelect } from "./js/Select.js";
-export { default as NooraTabs } from "./js/Tabs.js";
-export { default as NooraTagsInput } from "./js/TagsInput.js";
-export { default as NooraTooltip } from "./js/Tooltip.js";
+import { PlaceCursorAtEnd } from "./js/hooks.js";
+import NooraAvatar from "./js/Avatar.js";
+import NooraChart from "./js/Chart.js";
+import NooraCheckbox from "./js/Checkbox.js";
+import NooraCollapsible from "./js/Collapsible.js";
+import NooraDigitInput from "./js/DigitInput.js";
+import NooraDropdown from "./js/Dropdown.js";
+import NooraModal from "./js/Modal.js";
+import NooraPopover from "./js/Popover.js";
+import NooraSelect from "./js/Select.js";
+import NooraTabs from "./js/Tabs.js";
+import NooraTagsInput from "./js/TagsInput.js";
+import NooraTooltip from "./js/Tooltip.js";
+
+export default {
+  PlaceCursorAtEnd,
+  NooraAvatar,
+  NooraChart,
+  NooraCheckbox,
+  NooraCollapsible,
+  NooraDigitInput,
+  NooraDropdown,
+  NooraModal,
+  NooraPopover,
+  NooraSelect,
+  NooraTabs,
+  NooraTagsInput,
+  NooraTooltip,
+};
