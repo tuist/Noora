4acbf90605bf2244adc68bc54ffcc49229576e6b
Christoph Schmatzler <christoph@tuist.dev>
2025-06-03 17:39:05 +0200

    add settings
    Signed-off-by: Christoph Schmatzler <christoph@tuist.dev>

--- storybook/lib/noora_storybook_web/storybook.ex
+++ storybook/lib/noora_storybook_web/storybook.ex
@@ -1,9 +1,11 @@
 defmodule NooraStorybookWeb.Storybook do
   use PhoenixStorybook,
     otp_app: :noora_storybook_web,
+    title: "Noora Storybook",
     content_path: Path.expand("../../storybook", __DIR__),
-    # assets path are remote path, not local file-system paths
     css_path: "/assets/storybook.css",
     js_path: "/assets/storybook.js",
-    sandbox_class: "noora-storybook-web"
+    sandbox_class: "noora-storybook-web",
+    color_mode: true,
+    color_mode_sandbox_dark_class: "noora-dark"
 end
