63d7e155fb68b3dbaca7fea646e6a2f1d050d93e
Christoph Schmatzler <christoph@tuist.dev>
2025-06-04 17:13:21 +0200

    feat: add web package (#292)
    Signed-off-by: Christoph Schmatzler <christoph@tuist.dev>
    Co-authored-by: cschmatzler <8415974+cschmatzler@users.noreply.github.com>

--- cli/CHANGELOG.md
+++ cli/CHANGELOG.md
@@ -8,7 +8,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [0.39.0] - 2025-06-03
 ### Details
 #### Features
-- Initialise monorepo by @cschmatzler
+- Initialise monorepo by @cschmatzler in [#290](https://github.com/tuist/Noora/pull/290)
 
 ## [0.38.1] - 2025-05-29
 ### Details
---
+++ web/js/index.js
@@ -0,0 +1,33 @@
+import "../css/index.css";
+
+import { PlaceCursorAtEnd } from "./hooks.js";
+import NooraAvatar from "./Avatar.js";
+import NooraChart from "./Chart.js";
+import NooraCheckbox from "./Checkbox.js";
+import NooraCollapsible from "./Collapsible.js";
+import NooraDigitInput from "./DigitInput.js";
+import NooraDropdown from "./Dropdown.js";
+import NooraModal from "./Modal.js";
+import NooraPopover from "./Popover.js";
+import NooraSelect from "./Select.js";
+import NooraTabs from "./Tabs.js";
+import NooraTagsInput from "./TagsInput.js";
+import NooraTooltip from "./Tooltip.js";
+
+const Hooks = {
+  PlaceCursorAtEnd,
+  NooraAvatar,
+  NooraChart,
+  NooraCheckbox,
+  NooraCollapsible,
+  NooraDigitInput,
+  NooraDropdown,
+  NooraModal,
+  NooraPopover,
+  NooraSelect,
+  NooraTabs,
+  NooraTagsInput,
+  NooraTooltip,
+};
+
+export default { Hooks };
