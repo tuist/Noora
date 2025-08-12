4d9a566bab07b88c09ee8418eac14b55305bd61d
Christoph Schmatzler <christoph@schmatzler.com>
2025-08-12 12:05:02 +0000

    up elixir

--- web/mix.exs
+++ web/mix.exs
@@ -6,7 +6,7 @@ defmodule Noora.MixProject do
       app: :noora,
       description: "A component library for Phoenix LiveView applications",
       version: "0.11.1",
-      elixir: "~> 1.15",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
