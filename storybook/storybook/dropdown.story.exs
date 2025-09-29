195f7e6c89d02d71c84e333f298d104db09bcf88
fortmarek <marekfort@me.com>
2025-09-29 19:00:48 +0200

    fix: scroll dropdown options when too long in modal

--- storybook/storybook/dropdown.story.exs
+++ storybook/storybook/dropdown.story.exs
@@ -109,6 +109,37 @@ defmodule TuistWeb.Storybook.Dropdown do
           <.dropdown_item value="1" label="Item 1"/>
           """
         ]
+      },
+      %Variation{
+        id: :many_options,
+        description: "Dropdown with many options to force scrolling",
+        attributes: %{
+          label: "Many Options"
+        },
+        slots: [
+          """
+          <.dropdown_item value="1" label="Option 1" />
+          <.dropdown_item value="2" label="Option 2" />
+          <.dropdown_item value="3" label="Option 3" />
+          <.dropdown_item value="4" label="Option 4" />
+          <.dropdown_item value="5" label="Option 5" />
+          <.dropdown_item value="6" label="Option 6" />
+          <.dropdown_item value="7" label="Option 7" />
+          <.dropdown_item value="8" label="Option 8" />
+          <.dropdown_item value="9" label="Option 9" />
+          <.dropdown_item value="10" label="Option 10" />
+          <.dropdown_item value="11" label="Option 11" />
+          <.dropdown_item value="12" label="Option 12" />
+          <.dropdown_item value="13" label="Option 13" />
+          <.dropdown_item value="14" label="Option 14" />
+          <.dropdown_item value="15" label="Option 15" />
+          <.dropdown_item value="16" label="Option 16" />
+          <.dropdown_item value="17" label="Option 17" />
+          <.dropdown_item value="18" label="Option 18" />
+          <.dropdown_item value="19" label="Option 19" />
+          <.dropdown_item value="20" label="Option 20" />
+          """
+        ]
       }
     ]
   end
