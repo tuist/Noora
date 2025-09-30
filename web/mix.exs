c93c3b164df7158b37b7ec7027922ab5c4e80384
pepicrft <663605+pepicrft@users.noreply.github.com>
2025-09-30 15:16:54 +0000

    [Release] Noora Web web-0.30.0

--- web/mix.exs
+++ web/mix.exs
@@ -5,7 +5,7 @@ defmodule Noora.MixProject do
     [
       app: :noora,
       description: "A component library for Phoenix LiveView applications",
-      version: "0.29.2",
+      version: "0.30.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
